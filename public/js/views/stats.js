dc58e4f44416aea20d5e14f3b9b95c160cd3a821
Kirill Korolyov <kirill.korolyov@gmail.com>
2012-05-01 16:58:21 +0100

    Removed "Clean completed" button

--- public/js/views/stats.js
+++ public/js/views/stats.js
@@ -13,16 +13,6 @@ define([
       Tasks.bind('all', this.render, this);
     },
 
-    events: {
-      "click .clear a": "clearCompleted"
-    },
-
-    // Clear all done task items, destroying their models.
-    clearCompleted: function() {
-      _.each(Tasks.done(), function(task) { task.destroy(); });
-      return false;
-    },
-
     render: function() {
       var done = Tasks.done().length;
       var newTasks = Tasks.new().length;
