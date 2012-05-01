bba5dd737ad80bb51df8ecb9c53c58b0a596b6bc
Kirill Korolyov <kirill.korolyov@gmail.com>
2012-05-01 06:41:31 +0100

    Sprint task board doesn't allow adding tasks anymore

--- public/js/views/sprint.js
+++ public/js/views/sprint.js
@@ -10,15 +10,8 @@ define([
 
     template: _.template(sprintTemplate),
 
-    // Delegated events for creating new items, and clearing completed ones.
-    events: {
-      "keypress .new-task":  "createOnEnter",
-      "keyup .new-task":     "showTooltip"
-    },
-
     render: function() {
       this.$el.html(this.template());
-      this.input = this.$(".new-task");
       var tasks = this.$('.task-columns');
       tasks.find('.unstarted').append(new TasksView({
         filter: {status: 'new'}
@@ -31,34 +24,6 @@ define([
       }).render().$el);
       this.$('.task-stats').html(new StatsView().render().$el);
       return this;
-    },
-
-    // Generate the attributes for a new Task item.
-    newAttributes: function() {
-      return {
-        content: this.input.val(),
-        order:   Tasks.nextOrder()
-      };
-    },
-
-    // If you hit return in the main input field, create new **Task** model,
-    // persisting it to *localStorage*.
-    createOnEnter: function(e) {
-      if (e.keyCode != 13) return;
-      Tasks.create(this.newAttributes());
-      this.input.val('');
-    },
-
-    // Lazily show the tooltip that tells you to press `enter` to save
-    // a new task item, after one second.
-    showTooltip: function(e) {
-      var tooltip = this.$(".ui-tooltip-top");
-      var val = this.input.val();
-      tooltip.fadeOut();
-      if (this.tooltipTimeout) clearTimeout(this.tooltipTimeout);
-      if (val == '' || val == this.input.attr('placeholder')) return;
-      var show = function() { tooltip.show().fadeIn(); };
-      this.tooltipTimeout = _.delay(show, 1000);
     }
 
   });
